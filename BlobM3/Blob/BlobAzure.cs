136445fcb579e34ce4e963b0312cd5db82ffd749
edgardo.nvf <edgardo.nvf@gmail.com>
2021-05-26 14:54:05 -0500

    Blob Service

---
+++ BlobM3/Blob/BlobAzure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Azure.Storage.Blobs;
+
+namespace BlobM3.Blob
+{
+    /*Es un clase que se conecta al Servicio Blob Storage
+      Datos necesarios:
+      1.- Cadena de conexión
+     */
+    class BlobAzure
+    {
+        private static readonly string CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=storagevefer;AccountKey=zFBrGFqJWFHXEBIDxQCypACzmKh1NEqXjPF8I9CYfRa7CB1EmKy1fLZVhP46swfXQYaYG6iRR0SbOdltkdZe4Q==;EndpointSuffix=core.windows.net";
+
+        /*Variable para usar el servicio en cualquier parte de mi código*/
+        /*Traducción: Podrás obtener y usar el servicio en cualquier parte,
+         pero no lo podrás modificar fuera de la clase*/
+        public static BlobServiceClient Servicio { get; private set; }
+
+        /*Constructor: Nos ayuda a inicializar los atributos de una clase*/
+        static BlobAzure() { InitBlob(); }
+
+        /*Método usado para inicializar nuestro servicio*/
+        private static void InitBlob() {
+            if (Servicio == null) {
+                Servicio = new BlobServiceClient(CONNECTION_STRING);
+            }
+        }
+
+    }
+}
---
+++ BlobM3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Azure.Storage.Blobs;
+using BlobM3.Blob;
+using System.Linq;
+using System.IO;
+
+namespace BlobM3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Inicializamos el servicio de blob storage*/
+            var cliente = BlobAzure.Servicio;
+
+            Menu();
+            Console.WriteLine("Selecciona opción: ");
+            var opc = int.Parse(Console.ReadLine());
+            while (opc!=7) {
+                switch (opc) {
+                    case 1:
+                        Console.WriteLine("-.-.-.Contenedores-.-.-.");
+                        GetContainers(cliente);
+                        Menu();
+                        Console.WriteLine("Selecciona opción: ");
+                        opc = int.Parse(Console.ReadLine());
+                        break;
+
+                    case 2:
+                        Console.WriteLine("-.-.-Cargando Archivo-.-.-.");
+                        CargarArchivo(cliente);
+                        Menu();
+                        Console.WriteLine("Selecciona opción: ");
+                        opc = int.Parse(Console.ReadLine());
+                        break;
+                
+                }
+             
+            }
+
+        }
+
+        private static void Menu() {
+            Console.WriteLine("-.-.-.Menú Opciones-.-.-.");
+            Console.WriteLine("-.- 1.Ver Contenedores");
+            Console.WriteLine("-.-.2.Cargar archivo");
+            Console.WriteLine("-.-.-.-.Fin Menú-.-.-.-.");
+        }
+
+        private static void CargarArchivo(BlobServiceClient cliente) {
+            Console.WriteLine("Url archivo: ");
+            var url = Console.ReadLine();
+            Console.WriteLine("Nombre Contenedor: ");
+            var nombreC = Console.ReadLine();
+
+            /*validamos que el contenedor exista*/
+            var contenedor = cliente.GetBlobContainerClient(nombreC.ToLower());
+            if (contenedor.Exists()) {
+                var nombreArchivo = Path.GetFileName(url); //C:/archivo1.txt
+                try {
+                    var respuesta = contenedor.UploadBlob(nombreArchivo, File.OpenRead(url));
+                    Console.WriteLine(respuesta.GetRawResponse().Status);
+                    Console.WriteLine("cargado correctamente");
+                }
+                catch (Azure.RequestFailedException ex) {
+                    Console.WriteLine($"error al cargar archivo {ex}");
+                    return;
+                }
+
+            }
+        
+        }
+
+
+       
+        private static void GetContainers(BlobServiceClient cliente) {
+            /*obtenemos todos los contenedores de nuestro Blob Storage*/
+            var contenedores = cliente.GetBlobContainers();
+            contenedores.ToList()
+                        .ForEach(t=>Console.WriteLine($"nombre:{t.Name}"));
+        
+        }
+
+    }
+}
